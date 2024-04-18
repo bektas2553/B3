ee5766dbcf23b092969e1cf56e9417d1717abee0
ByB3ktaS <71300415+bektas2553@users.noreply.github.com>
2024-04-18 14:44:20 +0300

    Create b.py

---
+++ b.py
@@ -0,0 +1,20 @@
+import socket
+import socks
+
+server = "irc.kardelenfm.net"
+port = 6667
+nickname = "Bott"
+
+socks.set_default_proxy(socks.SOCKS5, "localhost", 1080)  # SOCKS5 proxy bilgilerini ayarlayın
+socket.socket = socks.socksocket  # Yeni bir socket oluşturulduğunda SOCKS5 proxy kullanılacak
+
+irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+irc.connect((server, port))
+irc.send(f"USER {nickname} {nickname} {nickname} :{nickname}\n".encode())
+irc.send(f"NICK {nickname}\n".encode())
+
+while True:
+    data = irc.recv(4096).decode()
+    print(data)
+    if data.find('PING') != -1:
+        irc.send('PONG ' + data.split() [1] + '\r\n')
