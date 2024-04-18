d093a9c1651459d034953dd22ce430d0645957f7
ByB3ktaS <71300415+bektas2553@users.noreply.github.com>
2024-04-18 16:06:50 +0300

    Update b.py

--- b.py
+++ b.py
@@ -3,19 +3,27 @@ import socks
 
 server = "irc.kardelenfm.net"
 port = 6667
-nickname = "Bott"
+link_part = "http://78.135.111.124:2138/"
 
-socks.set_default_proxy(socks.SOCKS5, "162.55.87.48", 5566)
+# SOCKS5 proxy ayarları
+proxy_host = "162.55.87.48"
+proxy_port = 5566  # SOCKS5 proxy portu
 
-socket.socket = socks.socksocket
+socks.set_default_proxy(socks.SOCKS5, proxy_host, proxy_port)
+socket.socket = socks.socksocket  # Yeni bir socket oluşturulduğunda SOCKS5 proxy kullanılacak
 
 irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 irc.connect((server, port))
-irc.send(f"USER {nickname} {nickname} {nickname} :{nickname}\n".encode())
-irc.send(f"NICK {nickname}\n".encode())
 
 while True:
-    data = irc.recv(4096).decode(encoding="ISO-8859-9")
+    data = irc.recv(4096).decode()
     print(data)
+    if link_part in data:
+        index = data.find(link_part) + len(link_part)
+        link = link_part
+        while index < len(data) and data[index] != ' ':
+            link += data[index]
+            index += 1
+        print("Bağlantı ve devamı:", link)
     if data.find('PING') != -1:
-        irc.send(f"PONG {data.split()[1]}\n".encode())
+        irc.send('PONG ' + data.split() [1] + '\r\n')
