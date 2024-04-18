b31e497143b43f1ac53d7f52a8b309a5abb62884
ByB3ktaS <71300415+bektas2553@users.noreply.github.com>
2024-04-18 17:19:47 +0300

    Update b.py

--- b.py
+++ b.py
@@ -3,23 +3,26 @@ import socks
 
 server = "irc.kardelenfm.net"
 port = 6667
+nickname = "Bott"
 link_part = "http://78.135.111.124:2138/"
 
-socks.set_default_proxy(socks.SOCKS5, '194.26.229.46', 20001)
-socket.socket = socks.socksocket  # Yeni bir socket oluşturulduğunda SOCKS5 proxy kullanılacak
+socks.set_default_proxy(socks.SOCKS5, "45.138.87.238", 1080)
+
+socket.socket = socks.socksocket
 
 irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 irc.connect((server, port))
+irc.send(f"USER {nickname} {nickname} {nickname} :{nickname}\n".encode())
+irc.send(f"NICK {nickname}\n".encode())
 
 while True:
-    data = irc.recv(4096).decode(encoding='ISO-8859-9')
+    data = irc.recv(4096).decode(encoding="ISO-8859-9")
     print(data)
     if data.find('PING') != -1:
-        irc.send('PONG ' + data.split() [1] + '\r\n')
+        irc.send(f"PONG {data.split()[1]}\n".encode())
+      
     if link_part in data:
         index = data.find(link_part) + len(link_part)
-        link = link_part
-        while index < len(data) and data[index] != ' ':
-            link += data[index]
-            index += 1
+        remaining_text = data[index:]
+        link = remaining_text.split(':')[0]
         print("Bağlantı ve devamı:", link)
