c69ef20e494b8ee1670ef03ba4ef8442c24d0f3e
ByB3ktaS <71300415+bektas2553@users.noreply.github.com>
2024-04-18 16:15:30 +0300

    Update b.py

--- b.py
+++ b.py
@@ -14,6 +14,8 @@ irc.connect((server, port))
 while True:
     data = irc.recv(4096).decode(encoding='ISO-8859-9')
     print(data)
+    if data.find('PING') != -1:
+        irc.send('PONG ' + data.split() [1] + '\r\n')
     if link_part in data:
         index = data.find(link_part) + len(link_part)
         link = link_part
@@ -21,5 +23,3 @@ while True:
             link += data[index]
             index += 1
         print("Bağlantı ve devamı:", link)
-    if data.find('PING') != -1:
-        irc.send('PONG ' + data.split() [1] + '\r\n')
